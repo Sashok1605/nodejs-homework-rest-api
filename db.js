5a40fe1ec119f43bebaa13fc31a2cce49f5b8116
Aleksandr Kulyk <ale.kulyk@gmail.com>
2022-01-28 17:23:24 +0200

    done

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+require('dotenv').config()
+
+mongoose
+  .connect(
+    process.env.DB_HOST_REMOTE
+  )
+  .then(() => console.log('Database connection successful'))
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+module.exports = mongoose;
